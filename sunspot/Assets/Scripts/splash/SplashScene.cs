5a336fd5913126adb3dde71726782fc4ab92edcc
Jesse Scott <jesse.scott@eventbase.com>
2017-03-16 23:19:13 -0700

    added existing unity project with splash screen

---
+++ sunspot/Assets/Scripts/splash/SplashScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScene : MonoBehaviour {
+
+	void Start () 
+	{
+		StartCoroutine("Countdown");
+	}
+
+	private IEnumerator Countdown()
+	{
+		yield return new WaitForSeconds(5);
+		Application.LoadLevel(1);
+	}
+	
+}
